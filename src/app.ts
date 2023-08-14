d2deb2a4dea1cea4c8c142a29b240ba1e11c5142
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-08-13 20:46:47 -0500

    :sparkles: create new endpoints for module dishes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,38 @@
 import express from "express";
 import http from "http";
 import morgan from "morgan";
+import bodyParser from "body-parser";
 
 const hostname = "localhost";
 const port = 3000;
 
 const app = express();
 app.use(morgan("dev"));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + "\\public\\"));
+
+app.all("/dishes", (_, res, next) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  next();
+});
+
+app.get("/dishes", (_, res) => {
+  res.send({ message: "Sending all the dishes" }).end();
+});
+
+app.post("/dishes", (req, res) => {
+  res.send({ message: "Adding the dishes", dish: req.body }).end();
+});
+
+app.put("/dishes", (req, res) => {
+  res.statusCode = 403;
+  res.send({ message: "Put operation not supported on /dishes" }).end();
+});
+
+app.delete("/dishes", (req, res) => {
+  res.send({ message: "Deleting dishes selected" }).end();
+});
 
 app.use((_, res) => {
   res.statusCode = 200;
