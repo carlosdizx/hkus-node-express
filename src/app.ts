65c50326522242a6df96d74c79c165a7711271fa
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-08-13 19:50:17 -0500

    :fire: remove unnecessary code

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import http from "http";
 import morgan from "morgan";
-import path from "path";
 
 const hostname = "localhost";
 const port = 3000;
@@ -10,10 +9,12 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "\\public\\"));
 
-app.use((req, res, next) => {
+app.use((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
-  res.end("<html><body><h1>This is an Express Server</h1></body></html>");
+  res.end(
+    '<html lang="es"><body><h1>This is an Express Server</h1></body></html>'
+  );
 });
 
 const server = http.createServer(app);
